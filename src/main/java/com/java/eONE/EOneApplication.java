3c550367fdc961c422c41169e3def99150507d76
Ajinkya-Dhavale <dhavaleajinkya9604@gmail.com>
2025-08-24 16:59:14 +0530

    print port

--- src/main/java/com/java/eONE/EOneApplication.java
+++ src/main/java/com/java/eONE/EOneApplication.java
@@ -4,16 +4,21 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
-public class EOneApplication {
+public class EOneApplication implements ApplicationListener<WebServerInitializedEvent> {
 
-	public static void main(String[] args) {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+        System.out.println("🚀 Starting Application...");
+        SpringApplication.run(EOneApplication.class, args);
+    }
 
-		System.out.println("hello");
-
-		SpringApplication.run(EOneApplication.class, args);
-	}
-
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        int port = event.getWebServer().getPort();
+        System.out.println("✅ Application is running on port: " + port);
+    }
 }
