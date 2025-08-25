66f8285a34d67e7c0f93f5400d22c4048fbb4b0c
Ajinkya-Dhavale <dhavaleajinkya9604@gmail.com>
2025-08-25 12:19:15 +0530

    add index.html

--- src/main/java/com/java/eONE/controller/EmptyRoutes.java
+++ src/main/java/com/java/eONE/controller/EmptyRoutes.java
@@ -1,18 +1,14 @@
 package com.java.eONE.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@CrossOrigin(origins = "*")
-@RequestMapping("/")
+@Controller
 public class EmptyRoutes {
     
     @GetMapping("/")
     public String getGello()
     {
-        return "hello";
+        return "index";
     }
 }
